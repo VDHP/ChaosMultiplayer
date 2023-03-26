777aabc8cb781f98eb6b662ecb1aee4d00bb668e
VDHP <99h2002@gmail.com>
2023-03-26 16:47:57 +0700

    Test

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -57,7 +57,7 @@ public class PlayerController : NetworkBehaviour , IKitchenObjectParent
     // Update is called once per frame
     void Update()
     {
-        if(!IsOwner) return;
+        
         HandleMovement();
         HandleInteract();
     }
