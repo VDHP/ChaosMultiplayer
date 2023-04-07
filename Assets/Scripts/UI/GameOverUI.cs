49e732c0e38c0e383f21f5bf3f859dee4d3bd351
VDHP <99h2002@gmail.com>
2023-04-07 10:57:16 +0700

    Done

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI recipeDeliveryText;
+    [SerializeField] Button PlayAgainButton;
     private void Start()
     {
         GameManager.Instance.OnStateChanged += Instance_OnStateChanged;
         Hide();
+    }
+    private void Awake()
+    {
+        PlayAgainButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.Shutdown();
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
     }
 
     private void Instance_OnStateChanged()
---
+++ Assets/Scripts/UI/HostDisconectUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HostDisconectUI : MonoBehaviour
+{
+    [SerializeField] Button PlayAgainButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkManager.Singleton.OnClientDisconnectCallback += Singleton_OnClientDisconnectCallback;
+
+        Hide();
+    }
+
+    private void Singleton_OnClientDisconnectCallback(ulong clientID)
+    {
+        if(clientID == NetworkManager.ServerClientId)
+        {
+            Show();
+        }
+    }
+
+    void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+    void Show()
+    {
+        gameObject.SetActive(true);
+    }
+}
